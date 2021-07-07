9ba5f87dd7e4c2b8f6e0c8104afc06f0ed48b95c
mzalw <76458819+mzalw@users.noreply.github.com>
2021-07-07 19:53:36 +0200

    Dodawanie danych z form do useState

--- src/components/admin/admin.js
+++ src/components/admin/admin.js
@@ -1,11 +1,68 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
+
+const Form = () => {
+    const [form, setForm] = useState({
+        id: 0,
+        title: "",
+        date: "",
+        description: ""
+    });
+
+    const SendPost = () => {
+
+        const dateNow = new Date();
+
+        const API = "http://localhost:3000";
+
+        fetch(`${API}/db`)
+            .then(response => response.json()).catch(error => console.log(error))
+            .then(data => {
+                setForm(prevState => {
+                    return {
+                        ...prevState,
+                        id: data.postsDB.length + 1,
+                        date: `0${dateNow.getDate()}.0${dateNow.getMonth() + 1}.${dateNow.getFullYear()}`
+                    }
+                })
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+    console.log(form.id);
+    console.log(form.title);
+    console.log(form.description);
+    console.log(form.date);
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setForm(prevState => {
+            return {
+                ...prevState,
+                [name]: value,
+            }
+        });
+    };
+    
+
+    return (
+        <>
+            <button onClick={SendPost}>Siema</button>
+            <form>
+                <input type="text" name="title" value={form.title} onChange={handleChange}/>
+                <input type="text" name="description" value={form.description} onChange={handleChange}/>
+            </form>
+        </>
+    );
+}
 
 const Admin = () => {
     return (
         <>
             <h1>Dodaj post</h1>
             <Link to={"/"}>Strona główna</Link>
+            <Form/>
         </>
     );
 }
